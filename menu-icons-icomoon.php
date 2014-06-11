8e88c9277db172530e30e5b5ee6e425e275f717a
Dzikri Aziz <kvcrvt@gmail.com>
2014-06-11 16:54:08 +0700

    Initial

---
+++ menu-icons-icomoon.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Menu Icons: IcoMoon
+ *
+ * @package Menu_Icons
+ * @subpackage IcoMoon
+ * @version 0.1.0
+ * @author Dzikri Aziz <kvcrvt@gmail.com>
+ *
+ *
+ * Plugin name: Menu Icons: Icomoon
+ * Plugin URI: http://github.com/kucrut/wp-menu-icons-icomoon
+ * Description: IcoMoon package for Menu Icons
+ * Version: 0.1.0
+ * Author: Dzikri Aziz
+ * Author URI: http://kucrut.org/
+ * License: GPLv2
+ * Text Domain: menu-icons-icomoon
+ * Depends: Menu Icons
+ */
+
+
+/**
+ * Main plugin class
+ */
+final class Menu_Icons_Icomoon {
+
+	/**
+	 * Load plugin
+	 *
+	 * @since   0.1.0
+	 * @wp_hook action plugins_loaded/99
+	 */
+	public static function _load() {
+		load_plugin_textdomain( 'menu-icons-icomoon', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+		if ( ! class_exists( 'Menu_Icons' ) ) {
+			add_action( 'admin_notices', array( __CLASS__, '_notice' ) );
+			return;
+		}
+
+		add_filter( 'menu_icons_types', array( __CLASS__, '_register' ) );
+	}
+
+
+	/**
+	 * Display admin notice when Menu Icons is not installed/activated
+	 *
+	 * @since   0.1.0
+	 * @wp_hook action admin_notice
+	 */
+	public static function _notice() {
+		?>
+		<div class="error">
+			<p><?php printf(
+				wp_kses_post( __( 'Please install and activate <a href="%s">Menu Icons</a> to use Icomoon.', 'menu-icons-icomoon' ) ),
+				'http://wordpress.org/plugins/menu-icons/'
+			) ?></p>
+		</div>
+		<?php
+	}
+
+
+	/**
+	 * Register type
+	 *
+	 * @since   0.1.0
+	 * @wp_hook filter menu_icons_types
+	 */
+	public static function _register( $icon_types ) {
+		require_once dirname( __FILE__ ) . '/icomoon.php';
+
+		$instance = new Menu_Icons_Type_Icomoon(
+			array(
+				'stylesheet' => sprintf(
+					'%scss/icomoon%s.css',
+					plugin_dir_url( __FILE__ ),
+					Menu_Icons::get_script_suffix()
+				),
+			)
+		);
+		$icon_types = $instance->register( $icon_types );
+
+		return $icon_types;
+	}
+}
+add_action( 'plugins_loaded', array( 'Menu_Icons_Icomoon', '_load' ), 99 );
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Menu Icons: IcoMoon ===
+Contributors: kucrut
+Donate Link: http://kucrut.org/#coffee
+Tags: menu, nav-menu, icons, navigation
+Requires at least: 3.8
+Tested up to: 3.9
+Stable tag: 0.1.0
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+IcoMoon extension for Menu Icons
+
+
+== Description ==
+
+[IcoMoon](http://icomoon.io/) extension for [Menu Icons](http://wordpress.org/plugins/menu-icons/).
+
+= Usage =
+1. After the plugin is activated, go to *Appearance* > *Menus* to edit your menus
+1. Enable *IcoMoon* in "Menu Icons Settings" meta box and click the save button
+1. Select icon by clicking on the "Select icon" link
+1. Save the menu
+
+
+== Screenshots ==
+1. Icon selection
+
+
+== Installation ==
+1. Install & activate [Menu Icons](http://wordpress.org/plugins/menu-icons/)
+1. Upload `menu-icons-icomoon` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the *Plugins* menu in WordPress
+
+
+== Other Notes ==
+Originally, the icon classes are prefixed with `icon-`. I think it's too common so I've changed it to `icomoon-` to avoid conflicts with other elements.
+
+
+== Changelog ==
+= 0.1.0 =
+* Initial release
